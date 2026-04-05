6e09883ea2d87ceec30a1c106d5e51a509c3c888
roughness1 <mikhail200720@gmail.com>
2026-04-05 22:53:49 +0300

    Update CloudApi.h

--- include/CloudApi.h
+++ include/CloudApi.h
@@ -6,25 +6,23 @@
 #include <nlohmann/json.hpp>
 #include "FileInfo.h"
 
-using json = nlohmann::json;
-
 class CloudApi {
 private:
     std::string apiToken;
-    std::string serverUrl = "https://www.googleapis.com/drive/v3/files";
-    std::string uploadUrl = "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart";
     bool isConnected;
 
+    std::string sendRequest(const std::string& url, 
+                           const std::string& method, 
+                           const std::string& body = "", 
+                           struct curl_slist* extraHeaders = nullptr);
+
     static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp);
-    std::string sendRequest(const std::string& url, const std::string& method, const std::string& body = "", struct curl_slist* extraHeaders = nullptr);
 
 public:
-   CloudApi(const std::string& token);
+    CloudApi(const std::string& token);
     ~CloudApi();
 
-    bool connect(); 
-    bool uploadFile(const FileInfo& file);
+    bool connect();
     std::vector<FileInfo> getCloudFiles();
-
-    //возможно bool deleteFile(const std::string& fileId);
+    bool uploadFile(FileInfo& file); 
 };
