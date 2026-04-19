0b4fb950dccd7de4075a1265c72dba15ff3afd25
roughness1 <mikhail200720@gmail.com>
2026-04-19 20:33:53 +0300

    Update CloudApi.h
    поддержка refresh token, изолированная папка для синхронизации

--- include/CloudApi.h
+++ include/CloudApi.h
@@ -11,22 +11,31 @@ namespace fs = std::filesystem;
 
 class CloudApi {
 public:
-    CloudApi(const std::string& token);
+    CloudApi(const std::string& token, const std::string& clientId, 
+         const std::string& clientSecret, 
+         const std::string& refreshToken);
     virtual ~CloudApi();
-
     bool connect();
     std::vector<FileInfo> getCloudFiles();
     bool uploadFile(const FileInfo& file);
     bool downloadFile(const std::string& fileName, const fs::path& destination);
 
 private:
+    std::string clientId;     // Твой Client ID из Google Console
+    std::string clientSecret; // Твой Client Secret
+    std::string refreshToken; // Тот самый "вечный" токен
+    // Метод для получения нового access_token
+    bool refreshAccessToken();
     std::string apiToken;
     bool isConnected;
+    std::string rootFolderId; // id изолированной папки
 
+    bool setupRootFolder(const std::string& folderName);
     std::string sendRequest(const std::string& url, const std::string& method, 
                             const std::string& body = "", struct curl_slist* extraHeaders = nullptr);
     std::string getFileIdByName(const std::string& fileName);
     std::string escapeString(const std::string& value);
+    
     static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp);
 };
 
