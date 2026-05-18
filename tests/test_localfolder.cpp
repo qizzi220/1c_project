0e1ced7ea6f7d2be52e8b663c3150e52971ed8ef
Данила Михалин <dmihalin4@gmail.com>
2026-05-18 15:00:07 +0300

    Update test_localfolder.cpp
    тесты 3,4,5 модуля localfolder

--- tests/test_localfolder.cpp
+++ tests/test_localfolder.cpp
@@ -45,3 +45,55 @@ TEST(LocalFolderTest, GetFilesReturnsCorrectNames) {
     
     fs::remove_all(testDir);
 }
+
+// setPath() меняет папку и пересканирует
+TEST(LocalFolderTest, SetPathChangesDirectory) {
+    fs::path dir1 = fs::temp_directory_path() / "cloudsync_test_set1";
+    fs::path dir2 = fs::temp_directory_path() / "cloudsync_test_set2";
+    
+    fs::create_directory(dir1);
+    fs::create_directory(dir2);
+    
+    std::ofstream(dir1 / "only_in_dir1.txt") << "data";
+    std::ofstream(dir2 / "only_in_dir2.txt") << "data";
+    
+    LocalFolder folder(dir1);
+    folder.scan();
+    EXPECT_EQ(folder.getFiles().size(), 1);
+    
+    folder.setPath(dir2);
+    EXPECT_EQ(folder.getFiles().size(), 1);
+    
+    fs::remove_all(dir1);
+    fs::remove_all(dir2);
+}
+
+// getPath() возвращает правильный путь
+TEST(LocalFolderTest, GetPathReturnsCorrectPath) {
+    fs::path testDir = fs::temp_directory_path() / "cloudsync_test_path";
+    fs::create_directory(testDir);
+    
+    LocalFolder folder(testDir);
+    
+    EXPECT_EQ(folder.getPath(), testDir);
+    
+    fs::remove_all(testDir);
+}
+
+// readFile() правильно читает содержимое файла
+TEST(LocalFolderTest, ReadFileReturnsCorrectContent) {
+    fs::path testDir = fs::temp_directory_path() / "cloudsync_test_read";
+    fs::create_directory(testDir);
+    
+    std::string expected = "Hello, CloudSync!";
+    std::ofstream(testDir / "test.txt") << expected;
+    
+    LocalFolder folder(testDir);
+    folder.scan();
+    std::vector<char> content = folder.readFile("test.txt");
+    std::string actual(content.begin(), content.end());
+    
+    EXPECT_EQ(actual, expected);
+    
+    fs::remove_all(testDir);
+}
