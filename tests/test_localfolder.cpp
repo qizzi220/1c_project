003de952bead4aa5c2346fa2437197c11a763af0
Данила Михалин <dmihalin4@gmail.com>
2026-05-18 15:29:05 +0300

    итоговые тесты модуля localfolder

--- tests/test_localfolder.cpp
+++ tests/test_localfolder.cpp
@@ -2,27 +2,32 @@
 #include "../include/LocalFolder.h"
 #include "../include/FileInfo.h"
 
+// Проверяем, что объект создается и не падает
 TEST(LocalFolderTest, InstanceCreation) {
     LocalFolder folder(".");
     ASSERT_NE(&folder, nullptr);
 }
 
+// Проверяем, что возвращается тот путь, который мы задали
 TEST(LocalFolderTest, GetPathReturnsPath) {
     LocalFolder folder("./test");
     EXPECT_EQ(folder.getPath(), "./test");
 }
 
+// Убеждаемся, что сканирование не ломается на пустой папке
 TEST(LocalFolderTest, ScanDoesNotCrash) {
     LocalFolder folder(".");
     EXPECT_NO_THROW(folder.scan());
 }
 
+// Получение списка файлов работает
 TEST(LocalFolderTest, GetFilesReturnsVector) {
     LocalFolder folder(".");
     folder.scan();
     EXPECT_TRUE(true);
 }
 
+// Если файла нет, возвращаем пустой результат
 TEST(LocalFolderTest, ReadFileForMissingFile) {
     LocalFolder folder(".");
     std::vector<char> result = folder.readFile("not_exist.txt");
